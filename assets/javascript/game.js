9159c857f529ad722156d85b9a5c50af2dc5737d
Edward Nguyen <ednguyen92@gmail.com>
2018-03-11 15:31:19 -0700

    Hit a wall with last approach trying something different involving more functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,41 @@
-// Define words that computer can pick from
-var computerChoices = ["pikachu", "squirtle", "charmander", "bulbasaur"];
+// GLOBAL Variables
 
-// Set the initial global variables
+//Array of Pokemon names to use
+var pokemonNames = ["pikachu", "squirtle", "charmander", "bulbasaur"];
+//Chosen name to guess 
+var choosenPokemon = "";
+//Holds letters in word
+var lettersInWord = [];
+//Holds number of blanks in word
+var num = 0;
+//Holds Blanks and successful guesses
+var blanksAndSuccesses =[];
+//Holds Wrong guesses
+var wrongLetters = [];
+//Counters
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var guessesMade = [];
+var rightGuessCounter = 0;
 
+// Functions
 
-      // Have computer pick a letter and store it in letterToBeGuessed
-      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-      console.log("Wins: " + wins + " Losses: " + losses + " GuessesLeft: " + " Guesses so far: " + guessesMade + " Computer picked: " + computerGuess);
-
-          // This function is run whenever the user presses a key.
-    document.onkeyup = function(event) {
-        // Determines which key was pressed.
-        var userGuess = event.key;
-
-        /* Somehow figure out how to avoid "Control" etc */
-         /*   if (event.charCode >= 97 && event.charCode <= 122)  {
-            guessesMade.push(userGuess);
-        } */
-
-        var hiddenAnswer = [];
-        var correctAnswer = 0;
-
-        guessesMade.push(userGuess);
-        
-        for (var i = 0; i < computerGuess.length; i++) {
-            /* computerGuess.charAt(i) is in guessesMade */
-            /* how do i check if something is in an array */
-            if (guessesMade.indexOf(computerGuess.charAt(i)) > -1) {
-            hiddenAnswer[i] = computerGuess.charAt(i);
-            correctAnswer++;
-            }else {
-                hiddenAnswer[i] = "_";  
-                guessesLeft--;             
-            } 
-        }
-
-        if (correctAnswer === hiddenAnswer.length) {
-            wins++;
-        }
-
-    
-
-        
-
-       
-
-
-
-
-    	// Displaying progress to HTML
-        var html = "<p><h1>HangMan</h1></p>" + 
-        "<p><h4>Guess what letter I\'m thinking of</h4></p>" + 
-        "<p><h4>Wins: " + wins + "</h4></p>" + 
-        "<p><h4>Losses: " + addRight + "</h4></p>" + 
-        hiddenAnswer.join(" ") +
-        "<p><h4>Guesses Left: " + guessesLeft + "</h4></p>" + 
-        "<p><h4>Your guesses so far: " + guessesMade + "</h4></p>";
-        // place html into the game ID
-        document.querySelector("#game").innerHTML = html;
-
-        }
+function restartGame()
+{
+	//Chooses word randombly from the wordBank
+	choosenPokemon = pokemonNames[Math.floor(Math.random() * pokemonNames.length)];
+	//Splits the choosen word into individual letters
+	lettersInWord = choosenPokemon.split('');
+	//Get the number of blanks
+	numBlanks = lettersInWord.length;
+	
+	//RESET
+	//===========================================================
+	letterGuessed = 0;
+	rightGuessCounter = 0;
+	guessesLeft = 9;
+	wrongLetters =[];
+	blanksAndSuccesses =[];
+	test=false;
+	startGame();
+}
